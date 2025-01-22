9cb9afcfe0de7ede7ce88484afcdecf27446f130
Nicolò Curioni <nicolo.curioni96@gmail.com>
2025-01-22 14:04:23 +0100

    Initial Commit

---
+++ GenmojiDemo/GenmojiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenmojiDemoApp.swift
+//  GenmojiDemo
+//
+//  Created by Nicolò Curioni on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct GenmojiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
